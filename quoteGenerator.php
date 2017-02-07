69cdd3b52751340c2cf99861f4a1224309d98263
Karla Guardado <kmg023@g.harvard.edu>
2017-02-07 14:33:29 -0500

    Adding files

---
+++ quoteGenerator.php
@@ -0,0 +1,13 @@
+#Quote generator for index.html page
+#Randomly pulls from a selection of 3 different quotes for each page load
+
+<?php
+
+$quotes =
+[
+  0 => "We shall never know all the good that a simple smile can do. -Mother Teresa",
+  1 => "Do not take life too seriously. You will never get out of it alive. -Elbert Hubbard",
+  2 => "It is during our darkest moments that we must focus and see the light. -Aristotle Onassis"
+]
+
+shuffle ($quotes);
