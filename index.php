d65155785e14c5c8e02cd52498ec6317286674e1
Karla Guardado <kmg023@g.harvard.edu>
2017-02-09 13:26:27 -0500

    Fixed typo

--- index.php
+++ index.php
@@ -16,7 +16,13 @@
 		<img src='images/me.jpg' alt='Karla Guardado'>
 
 		<p>
-			Hello, my name is Karla Guardado and welcome to my page. I am currently a Data Specialist at the Harvard-Smithsonian Center for Astrophycis. I am part of the Director's Office, we were manage some of the intricaces of running the Chandra X-ray Observatory. Before that, I was an undergrad at MIT. I am here to learn the art of creating dynamic web applications. I have some prior experience doing so, but I'd like to learn for myself how to create and use all components of the process from sratch.
+			Hello, my name is Karla Guardado and welcome to my page. I am currently a
+			Data Specialist at the Harvard-Smithsonian Center for Astrophycis. I am part
+			of the Director's Office, where we manage some of the intricaces of running
+			the Chandra X-ray Observatory. Before that, I was an undergrad at MIT.
+			I am here to learn the art of creating dynamic web applications. I have
+			some prior experience doing so, but I'd like to learn for myself how to
+			create and use all components of the process from sratch.
 		</p>
 
 		<h2>Quote of the day:</h2>
